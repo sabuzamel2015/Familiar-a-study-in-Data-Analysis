e57c770357e70353a8eaf940eb18759094f6fdaa
sabuzamel2015 <33916928+sabuzamel2015@users.noreply.github.com>
2018-01-31 20:24:18 -0500

    Update script.py
    This is were the statistical tests were performed in order to determine whether to reject a null hypothesis or not.

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ vein_pack_lifespans = familiar.lifespans(package='vein')
 # Performed the 1 Sample T-Test 
 vein_pack_test = veins, pval = ttest_1samp(vein_pack_lifespans, 71)
 
-# Used an if else statement based on the p-value
+# Used an if else statement based on the p-value after the 1 Sample T-Test has been performed.
 if (pval < 0.05):
     "The Vein Pack Is Proven To Make You Live Longer!"
 else:
@@ -28,17 +28,19 @@ artery_pack_lifespans = familiar.lifespans(package='artery')
 # Performed a 2 Sample T-Test
 package_comparison_results = ttest_ind(vein_pack_lifespans, artery_pack_lifespans)
 
-# Used an if else statement based on the p-value
+# Used an if else statement based on the p-value after the 2 Sample T-Test test has been performed
 if (pval < 0.05):
     "the Artery Package guarantees even stronger results!"
 else:
     "the Artery Package is also a great product!"
 
-
+# This table is from the familiar package. calculates low, medium and high levels of iron for veins and artery.
 iron_contingency_table = familiar.iron_counts_for_package()
 
+# Performed a chi-squared test 
 iron_pvalue = chi2, iron_pvalue, dof, expected = chi2_contingency(iron_contingency_table)
 
+# Used an if else statement in relation to the p-value after the chi-square test has been performed
 if (pval < 0.05):
     "The Artery Package Is Proven To Make You Healthier!"
 else:
